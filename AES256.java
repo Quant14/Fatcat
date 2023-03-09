809e143493d50729961c3a6cff8c8edb9a65d69e
WhyIsEveryNameTaken124 <74968938+WhyIsEveryNameTaken124@users.noreply.github.com>
2023-03-09 20:04:02 +0200

    Add files via upload

---
+++ AES256.java
@@ -0,0 +1,34 @@
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
+public class AES256{
+    private static final String SECRET_KEY = "secret_key_password_thing";
+    private static final String IV = "some long iv";
+
+    public static String encrypt(String strToEncrypt) {
+        try {
+            System.out.println("In encryption with: " + strToEncrypt);
+            byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            IvParameterSpec ivspec = new IvParameterSpec(iv);
+
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), IV.getBytes(), 13049, 256);
+            SecretKey tmp = factory.generateSecret(spec);
+            SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8)));
+        } catch (Exception e) {
+            System.out.println("There was an error with the encryption");
+        }
+        return null;
+    }
+}
