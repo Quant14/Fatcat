6b467a90b778f60154f6626bbb35cce11d1a5f86
Quant14 <igrgames05@gmail.com>
2023-03-09 11:20:58 +0200

    issue id: 1 added: initial spring boot project

---
+++ src/main/java/org/elsys/fatcatserver/FatcatServerApplication.java
@@ -0,0 +1,13 @@
+package org.elsys.fatcatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatcatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FatcatServerApplication.class, args);
+    }
+
+}
