6166f8c1e9312bbfca3496da430819c5c9c616e4
luchost <luchost@abv.bg>
2023-03-09 20:38:43 +0200

    IssueId:#1 Added: logic class and GuardDistribution class

---
+++ src/main/java/logic/GuardDistribution.java
@@ -0,0 +1,13 @@
+package logic;
+import java.util.List;
+import java.util.Map;
+
+
+
+public class GuardDistribution {
+    int number_of_sectors;
+    int number_of_guards;
+    List<Map> danger_of_sectors;
+
+    int total_danger;
+}
