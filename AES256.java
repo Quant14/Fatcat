d86e83609bf938cf298623e866de08f801cb6da9
WhyIsEveryNameTaken124 <74968938+WhyIsEveryNameTaken124@users.noreply.github.com>
2023-03-09 23:02:17 +0200

    Update AES256.java

--- AES256.java
+++ AES256.java
@@ -30,4 +30,22 @@ public class AES256{
         }
         return null;
     }
+    public static String decrypt(String strToDecrypt) {
+        try {
+            byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            IvParameterSpec ivspec = new IvParameterSpec(iv);
+
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), IV.getBytes(), 13049, 256);
+            SecretKey tmp = factory.generateSecret(spec);
+            SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+        } catch (Exception e) {
+            System.out.println("There was an error with the decryption");
+        }
+        return null;
+    }
 }
